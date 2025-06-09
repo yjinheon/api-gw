401f914ff53c3985f35d5bf918f08471a1f8d4d7
jinheonyoon <yjinheon@gmail.com>
2025-06-09 16:14:45 +0900

    feat: init test gw project

---
+++ src/main/java/com/example/apigw/ApiGwApplication.java
@@ -0,0 +1,13 @@
+package com.example.apigw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGwApplication.class, args);
+    }
+
+}
